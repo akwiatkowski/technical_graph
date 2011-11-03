a09b1b2eb1cff2b9529aa0c1cdfa99e3abb0153d
Aleksander Kwiatkowski <bobikx@poczta.fm>
2011-11-03 22:56:53 +0100

    color generating class fixed for rasem

--- lib/technical_graph/graph_color_library.rb
+++ lib/technical_graph/graph_color_library.rb
@@ -6,7 +6,8 @@ require 'singleton'
 
 class GraphColorLibrary
   include Singleton
-  
+
+  # rock solid colors
   # http://www.imagemagick.org/script/color.php
   BASIC_COLORS = [
     'blue',
@@ -15,37 +16,52 @@ class GraphColorLibrary
     'purple'
   ]
 
+  # other random picked up, SVG need #RGB and I'm too lazy :]
   ADDITIONAL_COLORS = [
-    'tomato',
-    'sienna1',
-    'chocolate2',
-    'DarkGoldenrod4',
-    'OliveDrab4',
-    'ForestGreen',
-    'turquoise',
-    'DarkCyan',
-    'CadetBlue4',
-    'DeepSkyBlue4',
-    'DodgerBlue2',
-    'CornflowerBlue',
-    'MidnightBlue',
-    'MediumPurple3',
-    'magenta4',
-    'orchid4',
-    'DeepPink3',
-    'PaleVioletRed4',
-    'firebrick3'
+    '#8B3626', #'tomato',
+    '#FF8247', #'sienna1',
+               #'chocolate2',
+               #'DarkGoldenrod4',
+               #'OliveDrab4',
+               #'ForestGreen',
+               #'turquoise',
+               #'DarkCyan',
+               #'CadetBlue4',
+               #'DeepSkyBlue4',
+               #'DodgerBlue2',
+               #'CornflowerBlue',
+               #'MidnightBlue',
+               #'MediumPurple3',
+               #'magenta4',
+               #'orchid4',
+               #'DeepPink3',
+               #'PaleVioletRed4',
+               #'firebrick3'
   ]
 
   FAIL_COLOR = 'black'
 
   def initialize
-    @colors = BASIC_COLORS + ADDITIONAL_COLORS.sort {rand}
+    @colors = BASIC_COLORS + ADDITIONAL_COLORS.sort { rand }
+  end
+
+  # not too bright
+  MAX_INTENSITY = 0xbb
+
+  # Best solution, create random color JIT
+  def random_color
+    str = "#"
+    3.times do
+      str += colour = "%02x" % (rand * MAX_INTENSITY)
+    end
+    return str
   end
 
   def get_color
     color = @colors.shift
-    return FAIL_COLOR if color.nil?
+    #return FAIL_COLOR if color.nil?
+    # better, create random colors just in time
+    return random_color if color.nil?
     return color
   end
 
