b2ef153ad10295a35425b9c41b61a70eeb0a9344
Aleksander Kwiatkowski <bobikx@poczta.fm>
2011-11-12 20:17:19 +0100

    new noise removal algorithm

--- lib/technical_graph/data_layer_processor_noise_removal.rb
+++ lib/technical_graph/data_layer_processor_noise_removal.rb
@@ -6,13 +6,18 @@ module DataLayerProcessorNoiseRemoval
   DEFAULT_NOISE_REMOVAL_LEVEL = 3
   DEFAULT_NOISE_REMOVAL_WINDOW_SIZE = 10
 
+  NOISE_COEFF = 1000
+  NOISE_POWER_COEFF = 8
+
   def noise_removal_initialize(options)
     @noise_removal = options[:noise_removal] == true
     @noise_removal_level = options[:noise_removal_level] || DEFAULT_NOISE_REMOVAL_LEVEL
     @noise_removal_window_size = options[:noise_removal_window_size] || DEFAULT_NOISE_REMOVAL_WINDOW_SIZE
+
+    @noise_threshold = Math.log(NOISE_COEFF / @noise_removal_level)
   end
 
-  attr_accessor :noise_removal_level, :noise_removal_window_size, :noise_removal
+  attr_accessor :noise_removal_level, :noise_removal_window_size, :noise_removal, :noise_threshold
 
   # Smooth values
   def noise_removal_process
@@ -53,19 +58,42 @@ module DataLayerProcessorNoiseRemoval
     i_from = noise_removal_window_from(i)
     i_to = noise_removal_window_to(i)
 
+
+    # y_mean = part_array.collect { |p| p.y }.float_mean
+    # # another algorithm
+    # noise_strength = (data[i].y - y_mean).abs / y_mean
+    # return noise_strength_enough?(noise_strength)
+
+    # calc. avg 'derivative'
+    avg_der = calc_avg_derivative(i_from, i_to)
+    current_der = calc_avg_derivative(i-1, i+1)
+
+    # safety
+    return false if avg_der == 0 or current_der == 0
+
+    current_level = Math.log( (current_der / avg_der) ** NOISE_POWER_COEFF ).abs
+    logger.debug "noise removal, avg der #{avg_der}, current #{current_der}, current lev #{current_level}, threshold #{noise_threshold}"
+    return current_level > noise_threshold
+  end
+
+  def calc_avg_derivative(i_from, i_to)
     part_array = data.clone_partial_w_fill(i_from, i_to)
-    y_mean = part_array.collect { |p| p.y }.float_mean
-
-    # another algorithm
-    noise_strength = (data[i].y - y_mean).abs / y_mean
-    return noise_strength_enough?(noise_strength)
+    derivatives = Array.new
+    (1...part_array.size).each do |i|
+      x_len = (part_array[i].x - part_array[i - 1].x).abs
+      y_len = (part_array[i].y - part_array[i - 1].y).abs
+      derivatives << (x_len / y_len).abs if x_len.abs > 0
+    end
+    avg_der = derivatives.float_mean
+    return avg_der
   end
 
-  # Some magic here, beware
-  def noise_strength_enough?(noise_strength)
-    threshold_strength = Math.log(@noise_removal_level)
-    return noise_strength > threshold_strength
-  end
+  ## Some magic here, beware
+  #def noise_strength_enough?(noise_strength)
+  #  threshold_strength = Math.log(@noise_removal_level)
+  #  logger.debug "Noise removal noise str #{noise_strength}, threshold #{threshold_strength}"
+  #  return noise_strength > threshold_strength
+  #end
 
 
 end
